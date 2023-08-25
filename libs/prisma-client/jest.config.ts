7c69d92499e867704f4a16b56f66b17303a02dee
kulgeyko93 <Kulgeyko93@mail.ru>
2023-08-25 21:57:55 +0300

    feat: prisma config

--- libs/prisma-client/jest.config.ts
+++ libs/prisma-client/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'account',
+  displayName: 'prisma-client',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/account',
+  coverageDirectory: '../../coverage/libs/prisma-client',
 };
